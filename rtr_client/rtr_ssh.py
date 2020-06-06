911d6dc445793b2be2398cb1a77c01f4e2857e6c
Martin Levy <martin@cloudflare.com>
2020-06-06 12:11:04 -0700

    pylint kept happy - well somewhat happy

--- rtr_client/rtr_ssh.py
+++ rtr_client/rtr_ssh.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""rtr_ssh"""
 
 import sys
 import os
@@ -6,25 +7,29 @@ import subprocess
 
 #
 # run this first to prime your known_hosts file
-#    ssh -N -T -l rpki -p 8283 rtr.rpki.cloudflare.com
-# 
+# ssh -N -T -l rpki -p 8283 rtr.rpki.cloudflare.com
+#
 def rtr_ssh(host, port):
+	"""rtr_ssh"""
 
-	ssh = subprocess.Popen(['ssh',
-					'-N',
-					'-T',
-					'-o StrictHostKeyChecking=Yes',
-					'-o PasswordAuthentication=Yes',
-					'-o PreferredAuthentications=password',
-					'-l rpki',
-					'-p %s' % port,
-					host
+	ssh = subprocess.Popen(
+					[
+									'ssh',
+									'-N',
+									'-T',
+									'-o StrictHostKeyChecking=Yes',
+									'-o PasswordAuthentication=Yes',
+									'-o PreferredAuthentications=password',
+									'-l rpki',
+									'-p %s' % port,
+									host
 					],
-				shell=False,
-				bufsize=-1,
-				stdin=subprocess.PIPE,
-				stdout=subprocess.PIPE,
-				stderr=subprocess.PIPE)
+					shell=False,
+					bufsize=-1,
+					stdin=subprocess.PIPE,
+					stdout=subprocess.PIPE,
+					stderr=subprocess.PIPE
+					)
 
 	#				'-v',
 	#				'-o Ciphers=ecdsa-sha2-nistp256',
@@ -43,9 +48,21 @@ def rtr_ssh(host, port):
 		if len(result) == 0:
 			error = ssh.stderr.readlines()
 			print('ERROR: %s' % (error))
-			exit(1)
+			sys.exit(1)
 
 		print(' LINE: %r' % (result))
 
-rtr_ssh('rtr.rpki.cloudflare.com', 8283)
+def doit(args):
+	"""rtr_ssh"""
 
+	rtr_ssh('rtr.rpki.cloudflare.com', 8283)
+
+def main(args=None):
+	"""rtr_ssh"""
+
+	if args is None:
+		args = sys.argv[1:]
+	doit(args)
+
+if __name__ == '__main__':
+	main()
